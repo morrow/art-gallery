b1c51720319cd6f2dd06b3f0a4c765793913bb1c
= <terrymorrow@gmail.com>
2015-08-24 18:30:49 -0400

    allow iframes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # allow iframes
+  config.action_dispatch.default_headers = {
+    'X-Frame-Options' => 'ALLOWALL'
+}
 end
